a75da9393c721394780062041d1c4674388e38d9
Trystan Partridge <partridget9@gmail.com>
2019-09-12 13:58:52 -0400

    update version to 2.0.3

--- src/clientversion.h
+++ src/clientversion.h
@@ -34,7 +34,7 @@
 // Modify version number here!!!
 #define CLIENT_VERSION_MAJOR 2
 #define CLIENT_VERSION_MINOR 0
-#define CLIENT_VERSION_REVISION 0
+#define CLIENT_VERSION_REVISION 3
 #define CLIENT_VERSION_BUILD 0
 
 
